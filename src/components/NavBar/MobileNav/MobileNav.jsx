71b505d263b587b3db030a3cd10f7293232e7564
Juan Contreras <juan.contreras.g123@gmail.com>
2024-03-30 16:25:06 -0300

    Creacion de la version mobile del navbar

---
+++ src/components/NavBar/MobileNav/MobileNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MobileNav.css'
+
+const MobileNav = ({isOpen, toggleMenu}) => {
+  return (
+    <>
+     <div 
+     
+     className='{mo}'
+     >
+
+     </div>
+    </>
+  )
+}
+
+export default MobileNav
