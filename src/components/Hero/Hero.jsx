0e79b499891797f762346561ac40faa68cb999c4
Juan Contreras <juan.contreras.g123@gmail.com>
2024-04-04 17:22:25 -0300

    creacion hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="hero-container">
+      <div className="hero-content">
+        <h2>Building Digital Experiences That Inspire</h2>
+        <p>
+          Passionate Fronted Developer | transforming Ideas into Seamless and
+          Visually Stunning Web Solution
+        </p>
+      </div>
 
-export default Hero
+      <div className="hero-image">
+        <div>
+          <div className="tech-icon">
+            <img src="../public\Screenshot_1.jpg" alt="React" />
+          </div>
+          <img src="images/react.svg" alt="" />
+        </div>
+        <div>
+          <div className="tech-icon">
+            <img src="images/node.svg" alt="Node" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
