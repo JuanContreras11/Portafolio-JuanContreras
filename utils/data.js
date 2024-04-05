466e289438466bf0969ac2570318056c30a3c764
Juan Contreras <juan.contreras.g123@gmail.com>
2024-04-05 13:03:02 -0300

    utilidades

---
+++ utils/data.js
@@ -0,0 +1,34 @@
+export const WORK_EXPERIENCE = [
+  {
+    title: "Senior frontend developer",
+    company: "Google",
+    Date: "january 2020- january 2021",
+    responsabilities: [
+      "lore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore",
+       "magna aliqulore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore" ,
+       "et dolore magna aliqua lore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    ],
+  },
+
+  {
+    title: "Senior frontend developer",
+    company: "Google",
+    Date: "january 2020- january 2021",
+    responsabilities: [
+      "lore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore",
+       "magna aliqulore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore" ,
+       "et dolore magna aliqua lore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    ],
+  },
+
+  {
+    title: "Senior frontend developer",
+    company: "Google",
+    Date: "january 2020- january 2021",
+    responsabilities: [
+      "lore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore",
+       "magna aliqulore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore" ,
+       "et dolore magna aliqua lore impsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    ],
+  },
+];
